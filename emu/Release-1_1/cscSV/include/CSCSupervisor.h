3083f5414c4c541ea8d00882dedc72f2a6649480
(no author) <(no author)>
2006-10-30 11:22:24 +0000

    This commit was manufactured by cvs2svn to create tag 'Release-1_1'.

---
+++ emu/Release-1_1/cscSV/include/CSCSupervisor.h
@@ -0,0 +1,80 @@
+#ifndef __CSC_SUPERVISOR_H__
+#define __CSC_SUPERVISOR_H__
+
+#include "EmuApplication.h"
+
+#include <string>
+
+#include "toolbox/fsm/FiniteStateMachine.h"
+#include "xdata/Vector.h"
+#include "xdata/Bag.h"
+#include "xdata/String.h"
+#include "xgi/Method.h"
+
+class CSCSupervisor : public EmuApplication
+{
+public:
+	XDAQ_INSTANTIATOR();
+
+	CSCSupervisor(xdaq::ApplicationStub *stub) throw (xdaq::exception::Exception);
+
+	// SOAP interface
+	xoap::MessageReference onConfigure(xoap::MessageReference message)
+			throw (xoap::exception::Exception);
+	xoap::MessageReference onEnable(xoap::MessageReference message)
+			throw (xoap::exception::Exception);
+	xoap::MessageReference onDisable(xoap::MessageReference message)
+			throw (xoap::exception::Exception);
+	xoap::MessageReference onHalt(xoap::MessageReference message)
+			throw (xoap::exception::Exception);
+
+	// HyperDAQ interface
+	void webDefault(xgi::Input *in, xgi::Output *out)
+			throw (xgi::exception::Exception);
+	void webConfigure(xgi::Input *in, xgi::Output *out)
+			throw (xgi::exception::Exception);
+	void webEnable(xgi::Input *in, xgi::Output *out)
+			throw (xgi::exception::Exception);
+	void webDisable(xgi::Input *in, xgi::Output *out)
+			throw (xgi::exception::Exception);
+	void webHalt(xgi::Input *in, xgi::Output *out)
+			throw (xgi::exception::Exception);
+
+	// State transitions
+	void configureAction(toolbox::Event::Reference e) 
+			throw (toolbox::fsm::exception::Exception);
+	void enableAction(toolbox::Event::Reference e) 
+			throw (toolbox::fsm::exception::Exception);
+	void disableAction(toolbox::Event::Reference e) 
+			throw (toolbox::fsm::exception::Exception);
+	void haltAction(toolbox::Event::Reference e) 
+			throw (toolbox::fsm::exception::Exception);
+
+private: // XDAQ parameters
+	xdata::Vector<xdata::String> config_keys_;
+	xdata::Vector<xdata::String> config_files_;
+
+private:
+	void stateChanged(toolbox::fsm::FiniteStateMachine &fsm)
+			throw (toolbox::fsm::exception::Exception);
+
+	void sendCommand(string command, string klass, int instance)
+			throw (toolbox::fsm::exception::Exception);
+	void setParameter(string klass, int instance,
+			string name, string type, string value)
+			throw (toolbox::fsm::exception::Exception);
+
+	string getRuntype(xoap::MessageReference message);
+	string getRuntype(xgi::Input *in);
+	string getRunNumber(xgi::Input *in);
+	string getNEvents(xgi::Input *in);
+
+	string runtype;
+	string runnumber;
+	string nevents;
+
+	string error_message_;
+};
+
+#endif  // ifndef __CSC_SUPERVISOR_H__
+// vim: set ai sw=4 ts=4:
