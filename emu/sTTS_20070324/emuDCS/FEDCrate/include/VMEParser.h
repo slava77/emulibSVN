85c28f1b5f1100250c8c5fbef5f02e4d13336f5b
(no author) <(no author)>
2007-03-23 12:44:19 +0000

    This commit was manufactured by cvs2svn to create tag 'sTTS_20070324'.

---
+++ emu/sTTS_20070324/emuDCS/FEDCrate/include/VMEParser.h
@@ -0,0 +1,55 @@
+//-----------------------------------------------------------------------
+// $Id: VMEParser.h,v 3.0 2006/07/20 21:16:10 geurts Exp $
+// $Log: VMEParser.h,v $
+// Revision 3.0  2006/07/20 21:16:10  geurts
+// *** empty log message ***
+//
+// Revision 1.2  2006/01/21 20:20:14  gilmore
+// *** empty log message ***
+//
+// Revision 1.8  2004/07/19 19:37:57  tfcvs
+// Removed unused variables and unsigned some variables in order to prevent compiler warnings (-Wall flag) (FG)
+//
+//
+//-----------------------------------------------------------------------
+#ifndef VMEParser_h
+#define VMEParser_h
+/*
+ *  class VMEParser
+ *  author Alex Tumanov 7/8/03
+ *     
+ */
+
+class Crate;
+class VMEController;
+#include "EmuParser.h"
+#include <xercesc/dom/DOM.hpp>
+
+
+XERCES_CPP_NAMESPACE_USE
+
+
+class VMEParser
+{
+
+
+public:
+  VMEParser(){}
+  explicit VMEParser(DOMNode * pNode, int number);
+
+  Crate *crate() const {return crate_;} 
+  VMEController * controller() const {return controller_;}
+
+protected:
+  Crate * crate_;
+  VMEController * controller_; 
+  EmuParser parser_;
+};
+
+#endif
+
+
+
+
+
+
