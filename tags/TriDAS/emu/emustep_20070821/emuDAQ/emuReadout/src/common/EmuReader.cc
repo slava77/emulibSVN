318775d398b7f159df2133bff141fb5d109f09e1
(no author) <(no author)>
2007-08-13 18:49:22 +0000

    This commit was manufactured by cvs2svn to create tag 'emustep_20070821'.

---
+++ tags/TriDAS/emu/emustep_20070821/emuDAQ/emuReadout/src/common/EmuReader.cc
@@ -0,0 +1,22 @@
+#include "EmuReader.h"
+
+int EmuReader::readNextEvent(){
+  // get this CPU hog out of here:     unsigned short ** buf2 = new unsigned short* ;
+  if ( theFormat == DDU ) theNumberOfReadBytes = readDDU( theBuffer );
+  else                    theNumberOfReadBytes = readDCC( theBuffer );
+  if ( theDebugMode ) std::cout << " theNumberOfReadBytes " << theNumberOfReadBytes << std::endl;
+  if ( theNumberOfReadBytes<=7 ) {
+    theErrorCount++;      
+    return theNumberOfReadBytes;
+  }
+  theErrorCount = 0;
+  // get this CPU hog out of here:    delete buf2;
+  // new and delete: ~400s/10^9 on a 2.4GHz Pentium 4 !!!
+  return theNumberOfReadBytes;
+}
+
+int EmuReader::eventNumber(){
+  // This should work for both DDU and DCC as LV1 is in the CDF part.
+  const MuEndDDUHeader * dduHeader = (const MuEndDDUHeader *) theBuffer;
+  return dduHeader->lvl1num();
+}
