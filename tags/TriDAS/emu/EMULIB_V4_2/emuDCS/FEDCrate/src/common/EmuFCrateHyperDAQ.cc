b6dcba72a557d308fc4937a91715d3cb33f02714
(no author) <(no author)>
2008-01-24 23:16:56 +0000

    This commit was manufactured by cvs2svn to create tag 'EMULIB_V4_2'.

---
+++ tags/TriDAS/emu/EMULIB_V4_2/emuDCS/FEDCrate/src/common/EmuFCrateHyperDAQ.cc
@@ -0,0 +1,18 @@
+// $Id: EmuFCrateHyperDAQ.cc,v 3.0 2006/07/20 21:16:11 geurts Exp $
+
+/*************************************************************************
+ * XDAQ Components for Distributed Data Acquisition                      *
+ * Copyright (C) 2000-2004, CERN.			                 *
+ * All rights reserved.                                                  *
+ * Authors: J. Gutleber and L. Orsini					 *
+ *                                                                       *
+ * For the licensing terms see LICENSE.		                         *
+ * For the list of contributors see CREDITS.   			         *
+ *************************************************************************/
+
+#include "EmuFCrateHyperDAQ.h"
+
+//
+// provides factory method for instantion of HellWorld application
+//
+XDAQ_INSTANTIATE(EmuFCrateHyperDAQ)
