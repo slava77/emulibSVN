19f15f52fd362f87fcb72b29436a67c0ea325b2e
(no author) <(no author)>
2008-08-12 10:51:46 +0000

    This commit was manufactured by cvs2svn to create tag 'EMULIB_V6_4'.

---
+++ emu/EMULIB_V6_4/emuDCS/FEDCrate/include/IRQData.h
@@ -0,0 +1,84 @@
+/* Global variables for DDU IRQ interrupt */
+#ifndef IRQDATA_h
+#define IRQDATA_h
+
+#include <time.h>
+#include <queue>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <pthread.h> // For mutexes
+
+#include "Crate.h"
+
+/** All of the information we need to know per IRQ error. **/
+class IRQError {
+
+public:
+
+	IRQError(Crate *myCrate, DDU *myDDU):
+		crate(myCrate),
+		ddu(myDDU),
+		fibers(0),
+		errorTime(0),
+		reset(0),
+		action("")
+	{
+		// Store the time when the error was recorded immediately.
+		time(&errorTime);
+	}
+
+	~IRQError() {}
+
+	// All public members for ease of access.
+	
+	Crate *crate;
+	DDU *ddu;
+	unsigned int fibers;
+	time_t errorTime;
+	unsigned long int reset;
+	std::string action;
+
+};
+
+
+/** The data that are handed off between the IRQ threads and the mother
+*	program.
+**/
+class IRQData {
+
+public:
+	IRQData(unsigned long int runNumber = 0):
+		exit(true),
+		runNumber(runNumber)
+	{
+		pthread_mutex_init(&crateQueueMutex, NULL);
+	}
+
+	~IRQData() {}
+
+	// Handle for the VME Controller this thread talks to.
+	//std::map<int,long> Handles;
+	
+	// A queue of crates that each thread can pop.  Mutexed.
+	pthread_mutex_t crateQueueMutex;
+	std::queue<Crate *> crateQueue;
+	
+	// "Global" variables -- all threads read these together.
+	bool exit;
+	unsigned long int runNumber;
+	
+	// "Local" variables -- each thread tries to increment only its own.
+	std::map<Crate *, unsigned long int> errorCount;
+	std::map<Crate *,DDU *> lastDDU;
+	std::map<Crate *,unsigned long int> ticks;
+	std::map<Crate *,time_t> tickTime;
+	std::map<Crate *,time_t> startTime;
+
+	std::map<Crate *,std::vector<IRQError *> > errorVectors;
+	
+};
+
+#endif
---
+++ emu/EMULIB_V6_4/emuDCS/FEDCrate/src/common/VMEParser.cc
@@ -0,0 +1,60 @@
+//-----------------------------------------------------------------------
+// $Id: VMEParser.cc,v 3.3 2008/06/10 13:52:12 gilmore Exp $
+// $Log: VMEParser.cc,v $
+// Revision 3.3  2008/06/10 13:52:12  gilmore
+// improved FED Crate HyperDAQ operability
+//
+// Revision 3.1  2007/07/23 05:03:31  gilmore
+// major structural chages to improve multi-crate functionality
+//
+// Revision 3.0  2006/07/20 21:16:11  geurts
+// *** empty log message ***
+//
+// Revision 1.3  2006/01/27 16:04:50  gilmore
+// *** empty log message ***
+//
+// Revision 1.8  2004/07/19 19:37:57  tfcvs
+// Removed unused variables and unsigned some variables in order to prevent compiler warnings (-Wall flag) (FG)
+//
+//
+//-----------------------------------------------------------------------
+#include "VMEParser.h"
+#include "VMEController.h"
+#include "Crate.h"
+
+VMEParser::VMEParser(DOMNode * pNode, int crate)
+{
+	int Link;
+	int Device;
+	parseNode(pNode);
+	fillInt("Device",Device); 
+	fillInt("Link",Link);
+	controller_ = new VMEController(Device, Link);
+	//fillInt("vmeirq_start", controller_->start_thread_on_init); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
