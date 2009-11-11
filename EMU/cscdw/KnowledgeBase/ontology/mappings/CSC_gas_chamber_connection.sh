8e938cee0ff8fb6983236d088b332b44e4b584d7
banicz <banicz>
2009-11-11 18:41:32 +0000

    From Valery's new, completed mapping.

--- EMU/cscdw/KnowledgeBase/ontology/mappings/CSC_gas_chamber_connection.sh
+++ EMU/cscdw/KnowledgeBase/ontology/mappings/CSC_gas_chamber_connection.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
-print "<!-- Generated from gas_chamber_connections.txt by gas_chamber_connections.sh -->"
+# Convert mapping text file to XML.
+# Usage: CSC_gas_chamber_connection.sh > GasSystem.xml
+MAPPINGFILE=CSC_gas_chamber_connection.txt
+print "<!-- Generated from $MAPPINGFILE by $0 -->"
 print "<gasSystem>"
-cat gas_chamber_connections.txt | while read LINE
+cat CSC_gas_chamber_connection.txt | while read LINE
 do
     if [[ $LINE =~ "Supply" ]]; then
 	LINEASARRAY=( $( echo $LINE) )
@@ -10,9 +13,11 @@ do
 	print "        <chamber name=\"${LINEASARRAY[4]}\"/>"
 	if [[ ${LINEASARRAY[9]} =~ "^ME[+-][1-4]/[1-3]/[0-3][0-9]$" ]]; then
 	    print "        <chamber name=\"${LINEASARRAY[9]}\"/>"
-	else
-	    print "        <chamber name=\"${LINEASARRAY[9]%/*}/${LINEASARRAY[9][8,9]}\"/>"
-	    print "        <chamber name=\"${LINEASARRAY[9]%/*}/${LINEASARRAY[9][11,12]}\"/>"
+	else 
+	    if [[ ${LINEASARRAY[9]} =~ "^ME[+-][1-4]/[1-3]/[0-3][0-9]-[0-3][0-9]$" ]]; then
+		print "        <chamber name=\"${LINEASARRAY[9]%/*}/${LINEASARRAY[9][8,9]}\"/>"
+		print "        <chamber name=\"${LINEASARRAY[9]%/*}/${LINEASARRAY[9][11,12]}\"/>"
+	    fi
 	fi
 	print "        <chamber name=\"${LINEASARRAY[11]}\"/>"
 	print "        <return gasRack=\"${LINEASARRAY[15]}\" pipeline=\"${LINEASARRAY[12,14]}\"/>"
