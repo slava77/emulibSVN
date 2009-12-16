6e0c71726eb81db5f9b789a63a0e39beac928cc0
valdo <valdo>
2009-12-16 17:05:13 +0000

    CDW: CdwModel project refactored to keep XML schemas and model classes; CdwWS project refactored to keep Cdw webservice and persistence configs

---
+++ EMU/cscdw/CdwWS/src/java/org/cern/cms/csc/dw/ws/FactCollectionInput.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cern.cms.csc.dw.ws;
+
+import org.cern.cms.csc.dw.model.FactCollectionType;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author evka
+ */
+@WebService()
+@Stateless()
+public class FactCollectionInput {
+
+    @PersistenceContext(name="CdwPU")
+    private EntityManager em;
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getFactCollection")
+    public Integer getFactCollection(@WebParam(name = "factCollection")
+    final FactCollectionType factCollection) {
+        if (factCollection == null) {
+            return 0;
+        }
+        try {
+            em.persist(factCollection);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 0;
+        }
+        return factCollection.getFact().size();
+    }
+
+}
