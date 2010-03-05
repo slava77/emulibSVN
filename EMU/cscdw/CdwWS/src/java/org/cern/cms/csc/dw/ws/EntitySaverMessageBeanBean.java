ed91fa9d8a0ac07f2b3b21563b4afd3524294acc
evka <evka>
2010-03-05 18:37:32 +0000

    FactColestionQueue renamed to FactColectionQueue. All services now make use of the PersistDao to save objects

---
+++ EMU/cscdw/CdwWS/src/java/org/cern/cms/csc/dw/ws/EntitySaverMessageBeanBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cern.cms.csc.dw.ws;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import org.cern.cms.csc.dw.dao.PersistDaoLocal;
+import org.cern.cms.csc.dw.exception.NotImplementedException;
+import org.cern.cms.csc.dw.exception.PersistException;
+import org.cern.cms.csc.dw.model.base.EntityBase;
+
+/**
+ *
+ * @author Evka
+ */
+@MessageDriven(mappedName = "jms/entitySaverQueue", activationConfig =  {
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+    })
+public class EntitySaverMessageBeanBean implements MessageListener {
+    
+    private static Logger logger = Logger.getLogger(EntitySaverMessageBeanBean.class.getName());
+
+    @EJB
+    private PersistDaoLocal persistDao;
+
+    public EntitySaverMessageBeanBean() {
+    }
+
+    public void onMessage(Message message) {
+        try {
+            ObjectMessage objMsg = (ObjectMessage) message;
+            Object entity = objMsg.getObject();
+            if (entity instanceof EntityBase) {
+                persistDao.persist((EntityBase) entity);
+            } else {
+                String className = "null";
+                if (entity != null) {
+                    className = objMsg.getObject().getClass().getCanonicalName();
+                }
+                logger.log(Level.SEVERE, className + " does not extend EntityBase class - cannot persist.");
+            }
+        } catch (PersistException pex) {
+            logger.log(Level.WARNING, "Cannot persist entity, stopping persistance queue. ", pex);
+            throw new NotImplementedException("Queue stopping is not yet implemented!");
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "Exception while persisting an entity: ", ex);
+        }
+        //persistDao
+    }
+    
+}
--- EMU/cscdw/CdwWS/src/java/org/cern/cms/csc/dw/ws/FactCollectionSaverBean.java
+++ EMU/cscdw/CdwWS/src/java/org/cern/cms/csc/dw/ws/FactCollectionSaverBean.java
@@ -5,12 +5,21 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.Session;
 import javax.xml.bind.JAXBElement;
 import org.cern.cms.csc.dw.exception.ComponentTypeNotAllowedInFactException;
 import org.cern.cms.csc.dw.dao.OntologyDaoLocal;
-import org.cern.cms.csc.dw.dao.PersistDaoLocal;
+import org.cern.cms.csc.dw.model.base.EntityBase;
 import org.cern.cms.csc.dw.model.fact.Fact;
 import org.cern.cms.csc.dw.model.fact.FactCollection;
 import org.cern.cms.csc.dw.model.fact.FactCollectionFactsItem;
@@ -19,14 +28,15 @@ import org.cern.cms.csc.dw.service.ServiceInstructions;
 
 @Stateless
 public class FactCollectionSaverBean implements FactCollectionSaverLocal {
+    @Resource(name = "jms/entitySaverQueue")
+    private Queue entitySaverQueue;
+    @Resource(name = "jms/entitySaverQueueFactory")
+    private ConnectionFactory entitySaverQueueFactory;
 
     private static Logger logger = Logger.getLogger(FactCollectionSaverBean.class.getName());
 
     @EJB
     private OntologyDaoLocal ontologyDao;
-
-    @EJB
-    private PersistDaoLocal persistDao;
 
     public void saveFactCollection(FactCollection factCollection) throws Exception {
 
@@ -81,9 +91,37 @@ public class FactCollectionSaverBean implements FactCollectionSaverLocal {
 
         // Persist collection
         if (instructions.isPersist()) {
-            persistDao.persist(factCollection);
+            sendJMSMessageToEntitySaverQueue(factCollection);
         }
 
+    }
+
+    private Message createJMSMessageForjmsEntitySaverQueue(Session session, EntityBase entity) throws JMSException {
+        ObjectMessage om = session.createObjectMessage();
+        om.setObject(entity);
+        return om;
+    }
+
+    private void sendJMSMessageToEntitySaverQueue(EntityBase entity) throws JMSException {
+        Connection connection = null;
+        Session session = null;
+        try {
+            connection = entitySaverQueueFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer messageProducer = session.createProducer(entitySaverQueue);
+            messageProducer.send(createJMSMessageForjmsEntitySaverQueue(session, entity));
+        } finally {
+            if (session != null) {
+                try {
+                    session.close();
+                } catch (JMSException e) {
+                    Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Cannot close session", e);
+                }
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
 }
