3a750c54730f738eedfc86b77dce13570e52ea19
(no author) <(no author)>
2010-08-21 08:54:03 +0000

    This commit was manufactured by cvs2svn to create tag 'EMULIB_V9_10'.

---
+++ tags/TriDAS/emu/EMULIB_V9_10/emuDCS/FEDApps/include/emu/fed/AppsVersion.h
@@ -0,0 +1,28 @@
+/*****************************************************************************\
+* $Id: AppsVersion.h,v 1.56 2010/07/19 16:34:03 liu Exp $
+\*****************************************************************************/
+
+#ifndef __EMU_FED_APPSVERSION_H__
+#define __EMU_FED_APPSVERSION_H__
+
+#include "config/PackageInfo.h"
+
+namespace EmuFEDApps {
+	const std::string package     = "EmuFEDApps";
+	const std::string versions    = "09.10.00";
+	const std::string description = "Emu (CSC) FED XDAQ Applications";
+
+	const std::string summary     = "Applications to control and monitor the Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
+	const std::string authors     = "Phillip Killewald, Stan Durkin, Jason Gilmore, Jianhui Gu";
+	const std::string link        = "http://www.physics.ohio-state.edu/~cms";
+
+	config::PackageInfo getPackageInfo();
+
+	void checkPackageDependencies()
+	throw (config::PackageInfo::VersionException);
+
+	std::set<std::string, std::less<std::string> > getPackageDependencies();
+}
+
+#endif
+
---
+++ tags/TriDAS/emu/EMULIB_V9_10/emuDCS/PeripheralApps/include/emu/pc/CrateUtilities.h
@@ -0,0 +1,81 @@
+#ifndef CrateUtilities_h
+#define CrateUtilities_h
+
+#include <iostream>
+#include <fstream>
+
+
+#include "emu/pc/Crate.h"
+
+namespace emu {
+  namespace pc {
+
+
+class CrateUtilities {
+  //
+public:
+  //
+  CrateUtilities();
+  virtual ~CrateUtilities();
+  //
+  inline void SetCrate(Crate * crate) { myCrate_ = crate ; }
+  //
+  inline void RedirectOutput(std::ostream * Output) { MyOutput_ = Output ; }
+  //
+  void MpcTMBTest(int number_of_events, int minimum_delay_value, int maximum_delay_value); //"safe window" scan
+  void MpcTMBTest(int number_of_events);                                                   //not the "safe window" scan
+  inline int  GetMpcTMBTestResult() { return MpcTMBTestResult; }             //result of the non-"safe window" scan
+  inline void SetMpcTMBTestResult(int result) { MpcTMBTestResult = result; }
+  //
+  void CreateTstoreTables();
+  void DumpTstoreTables();
+  //
+  inline std::vector< std::string> GetPeriphTable(){
+    return periph_table;
+  }
+  //
+  inline std::vector< std::string> GetCSCTable(int number){
+    return csc_table[number];
+  }
+  //
+  inline std::vector< std::string> GetTmbTable(int number){
+    return tmb_table[number];
+  }
+  //
+  inline std::vector< std::string> GetDmbTable(int number){
+    return dmb_table[number];
+  }
+  // 
+  inline std::vector< std::string> GetAlctTable(int number){
+    return alct_table[number];
+  }
+  //
+  inline std::vector< std::vector< std::string> > GetAfebTable(){
+    return afeb_table;
+  }
+  //
+  inline std::vector< std::vector< std::string> > GetCfebTable(){
+    return cfeb_table;
+  }
+  //
+ private:
+  //
+  bool debug_;
+  //
+  int MpcTMBTestResult;
+  std::ostream * MyOutput_ ;
+  //
+  Crate * myCrate_;
+  std::vector< std::string> periph_table;
+  std::vector< std::string> csc_table[9];
+  std::vector< std::string> tmb_table[9];
+  std::vector< std::string> dmb_table[9];
+  std::vector< std::string> alct_table[9];
+  std::vector< std::vector< std::string> > afeb_table;
+  std::vector< std::vector< std::string> > cfeb_table;
+  //
+};
+
+  } // namespace emu::pc
+  } // namespace emu
+#endif
