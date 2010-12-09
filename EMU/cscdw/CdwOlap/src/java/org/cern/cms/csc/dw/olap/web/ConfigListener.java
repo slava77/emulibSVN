d3ee0e38a14f40e2de69a11038ed55ef0b15c4e4
valdo <valdo>
2010-12-09 15:08:09 +0000

    System.out, etc. replaced with log4j Logger; log renamed to logger

--- EMU/cscdw/CdwOlap/src/java/org/cern/cms/csc/dw/olap/web/ConfigListener.java
+++ EMU/cscdw/CdwOlap/src/java/org/cern/cms/csc/dw/olap/web/ConfigListener.java
@@ -1,19 +1,20 @@
 package org.cern.cms.csc.dw.olap.web;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import org.apache.log4j.Logger;
 
 public class ConfigListener implements ServletContextListener, HttpSessionListener {
+
+    private static final Logger logger = Logger.getLogger(ConfigListener.class);
 
     public void contextInitialized(ServletContextEvent sce) {
         try {
             ConfigManager.init(sce.getServletContext().getRealPath("."));
         } catch (Exception ex) {
-            Logger.getLogger(ConfigListener.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(null, ex);
         }
     }
 
