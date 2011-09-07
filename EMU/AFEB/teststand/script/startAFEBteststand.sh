39dacb3c76c1cab7d6b574a9a5b96e7305fa36c0
banicz <banicz>
2011-09-07 15:47:32 +0000

    Guess more parameter values.

--- EMU/AFEB/teststand/script/startAFEBteststand.sh
+++ EMU/AFEB/teststand/script/startAFEBteststand.sh
@@ -1,17 +1,36 @@
 #!/bin/zsh
 
-export BUILD_HOME=/home/csctest/EMU
-export XDAQ_ROOT=/home/csctest/XDAQ
+# BUILD_HOME, XDAQ_ROOT and ROOTSYS should be set. Their default values are:
+export BUILD_HOME=${BUILD_HOME:-$HOME/EMU}
+export XDAQ_ROOT=${XDAQ_ROOT:-$HOME/XDAQ}
+export ROOTSYS=${ROOTSYS:-$HOME/ROOT/root}
+
 export XDAQ_OS=linux
-export XDAQ_PLATFORM=x86
-
+[[ $(uname -m) == "x86_64" ]] && export XDAQ_PLATFORM="x86_64" || export XDAQ_PLATFORM="x86"
 export XDAQ_DOCUMENT_ROOT=${XDAQ_ROOT}/${XDAQ_PLATFORM}/htdocs
+export LD_LIBRARY_PATH=$ROOTSYS/lib:$XDAQ_ROOT/$XDAQ_PLATFORM/lib
 
-#ddd ${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe
-# -h emuslice03.cern.ch -p 10000 -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile
+print
+print "Environment variables:"
+print "    BUILD_HOME         = $BUILD_HOME"
+print "    XDAQ_ROOT          = $XDAQ_ROOT"
+print "    XDAQ_OS            = $XDAQ_OS"
+print "    XDAQ_PLATFORM      = $XDAQ_PLATFORM"
+print "    XDAQ_DOCUMENT_ROOT = $XDAQ_DOCUMENT_ROOT"
+print "    ROOTSYS            = $ROOTSYS"
+print "    LD_LIBRARY_PATH    = $LD_LIBRARY_PATH"
+print "Executing:"
+print "${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe \
+ -h $(hostname) \
+ -p 10000 \
+ -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml \
+ -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile"
 
 ${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe \
-    -h emuslice03.cern.ch \
+    -h $(hostname).cern.ch \
     -p 10000 \
     -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml \
     -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile
+
+#ddd ${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe 
+# -h emuslice03.cern.ch -p 10000 -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile
