38f329032f94ce1ae0095d6eb12b930274af95c5
banicz <banicz>
2011-09-08 15:37:33 +0000

    Improved style of exception stack table.

--- EMU/AFEB/teststand/src/common/Xcept.cc
+++ EMU/AFEB/teststand/src/common/Xcept.cc
@@ -5,16 +5,16 @@ string AFEB::teststand::utils::xhtmlformat_exception_history( xcept::Exception& 
   stringstream ss;
   ss << "<table class=\"Xcept\">";
   ss << "<tr><th colspan=\"3\">Exception stack</th></tr>";
-  ss << "<tr><th>layer</th><th>message</th><th>raised at</th></tr>";
+  ss << "<tr><th style=\"width: 1%;\">layer</th><th>message</th><th>raised at</th></tr>";
 
   std::vector<xcept::ExceptionInformation> & history = e.getHistory();
   std::vector<xcept::ExceptionInformation>::reverse_iterator i = history.rbegin();
   int iLayer = history.size();
   while ( i != history.rend() ){
     ss << "<tr>";
-    ss << "<td>" << iLayer << "</td>";
+    ss << "<td style=\"width: 1%;\">" << iLayer << "</td>";
     ss << "<td style=\"background-color: #dddd11;\"><![CDATA[" << i->getProperty("message") << "]]></td>";
-    ss << "<td>" << i->getProperty("function") << " (" << i->getProperty("module") << ":" << i->getProperty("line") << ")</td>";
+    ss << "<td style=\"text-align: left;\">" << i->getProperty("function") << " (" << i->getProperty("module") << ":" << i->getProperty("line") << ")</td>";
     ss << "</tr>";
     ++i;
     --iLayer;
