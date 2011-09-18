b578f267567902d7ef400d28f071ea46ebbbf6d1
banicz <banicz>
2011-09-18 16:34:20 +0000

    Added option to run in debugger.

--- EMU/AFEB/teststand/script/startAFEBteststand.sh
+++ EMU/AFEB/teststand/script/startAFEBteststand.sh
@@ -1,5 +1,21 @@
 #!/bin/zsh
 
+# user's options
+while [[ $1 = -* ]]; do
+    case $1 in 
+        -d ) RUNINDEBUGGER="true"
+            shift ;;
+         * ) cat <<EOF
+Usage:
+    $0 [-d]
+
+Options:
+    -d    Run in a debugger (ddd)
+EOF
+	    exit 1 ;;
+    esac
+done
+
 # BUILD_HOME, XDAQ_ROOT and ROOTSYS should be set. Their default values are:
 export BUILD_HOME=${BUILD_HOME:-$HOME/EMU}
 export XDAQ_ROOT=${XDAQ_ROOT:-$HOME/XDAQ}
@@ -19,19 +35,22 @@ print "    XDAQ_PLATFORM      = $XDAQ_PLATFORM"
 print "    XDAQ_DOCUMENT_ROOT = $XDAQ_DOCUMENT_ROOT"
 print "    ROOTSYS            = $ROOTSYS"
 print "    LD_LIBRARY_PATH    = $LD_LIBRARY_PATH"
-print "Executing:"
-print "${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe \
+print
+
+OPTIONS="\
  -h localhost \
  -p 10000 \
  -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml \
  -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile"
-print
 
-${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe \
-    -h localhost \
-    -p 10000 \
-    -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml \
-    -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile
-
-#ddd ${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe 
-# -h emuslice03.cern.ch -p 10000 -c ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.xml -e ${BUILD_HOME}/AFEB/teststand/xml/AFEBteststand.profile
+if [[ $RUNINDEBUGGER = "true" ]]; then
+    print "Execute in ddd the command:"
+    print "run $OPTIONS"
+    print
+    eval "ddd ${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe"
+else
+    print "Executing:"
+    print "${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe $OPTIONS"
+    print
+    eval "${XDAQ_ROOT}/${XDAQ_PLATFORM}/bin/xdaq.exe $OPTIONS"
+fi
