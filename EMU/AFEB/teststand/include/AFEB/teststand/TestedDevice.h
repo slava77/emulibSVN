9a1150333a69cb41486fa38cc915083cb6b1b03c
banicz <banicz>
2012-02-09 18:53:58 +0000

    Incorporate Device into TestedDevice and let TestedDevice be the base class.

--- EMU/AFEB/teststand/include/AFEB/teststand/TestedDevice.h
+++ EMU/AFEB/teststand/include/AFEB/teststand/TestedDevice.h
@@ -16,12 +16,32 @@ namespace AFEB { namespace teststand {
     class TestedDevice;
     ostream& operator<<( ostream& os, const TestedDevice& d );
 
-    class TestedDevice : public Device {
+    class TestedDevice {
     public:
       friend ostream& operator<<( ostream& os, const TestedDevice& d );
       TestedDevice( const string type, const int nChannels, Crate* const crate );
+      void setParameters( const vector< pair<string,string> >& param );
+      int getTDCSlot() const { return tdcSlot_; }
+      int getSignalConverterSlot() const { return signalConverterSlot_; }
+      int getSignalConverterSocket() const { return signalConverterSocket_; }
+      int getPulseGeneratorSlot() const { return pulseGeneratorSlot_; }
+      int getPulseGeneratorSocket() const { return pulseGeneratorSocket_; }
+      int getTDCSocket() const { return tdcSocket_; }
+      string getId() const { return id_; }
+      string getType() const { return type_; }
+      int getNChannels() const { return nChannels_; }
       Crate* getCrate() const { return crate_; }
     protected:
+      string type_;
+      int nChannels_;
+      string id_;
+      int socket_;
+      int tdcSlot_;
+      int tdcSocket_;
+      int signalConverterSlot_;
+      int signalConverterSocket_;
+      int pulseGeneratorSlot_;
+      int pulseGeneratorSocket_;
       Crate* crate_;
     };
 
--- EMU/AFEB/teststand/src/common/TestedDevice.cc
+++ EMU/AFEB/teststand/src/common/TestedDevice.cc
@@ -20,5 +20,21 @@ ostream& AFEB::teststand::operator<<( ostream& os, const TestedDevice& d ){
 }
 
 AFEB::teststand::TestedDevice::TestedDevice( const string type, const int nChannels, Crate* const crate ) :
-  Device( type, nChannels ),
+  type_( type ),
+  nChannels_( nChannels ),
   crate_( crate ){}
+
+void AFEB::teststand::TestedDevice::setParameters( const vector< pair<string,string> >& param ){
+  vector< pair<string,string> >::const_iterator p;
+  for ( p = param.begin(); p != param.end(); ++p ){
+    if      ( p->first.compare( "c:id"       ) == 0 ) id_       = p->second;
+    else if ( p->first.compare( "c:socket"   ) == 0 ) socket_   = utils::stringTo<int>( p->second );
+    else if ( p->first.compare( "c:tdcSlot"  ) == 0 ) tdcSlot_  = utils::stringTo<int>( p->second );
+    else if ( p->first.compare( "c:tdcSocket" ) == 0 ) tdcSocket_ = utils::stringTo<int>( p->second );
+    else if ( p->first.compare( "c:signalConverterSlot"  ) == 0 ) signalConverterSlot_  = utils::stringTo<int>( p->second );
+    else if ( p->first.compare( "c:signalConverterSocket" ) == 0 ) signalConverterSocket_ = utils::stringTo<int>( p->second );
+    else if ( p->first.compare( "c:pulseGeneratorSlot"    ) == 0 ) pulseGeneratorSlot_    = utils::stringTo<int>( p->second );
+    else if ( p->first.compare( "c:pulseGeneratorSocket"  ) == 0 ) pulseGeneratorSocket_  = utils::stringTo<int>( p->second );
+  }
+  //cout << param << endl << *this << endl;
+}
