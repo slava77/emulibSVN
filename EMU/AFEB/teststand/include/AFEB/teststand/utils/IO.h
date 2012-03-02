ae2a09ff97d7120fb80e6795cf52889c3755957f
banicz <banicz>
2012-03-02 16:32:28 +0000

    Added insertion operator for sets.

--- EMU/AFEB/teststand/include/AFEB/teststand/utils/IO.h
+++ EMU/AFEB/teststand/include/AFEB/teststand/utils/IO.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <map>
+#include <set>
 #include <valarray>
 #include <iostream>
 
@@ -16,6 +17,16 @@ namespace AFEB { namespace teststand {
       typename vector<T>::const_iterator it;
       for ( it = t.begin(); it != t.end(); ++it ) cout << *it << (it+1 != t.end() ? "," : "");
       cout << "]";
+      return os;
+    }
+    
+    template <typename T>
+    ostream& operator<<( ostream& os, const set<T>& t ){
+      cout << "{";
+      typename set<T>::iterator it, iti;
+      for ( it = iti = t.begin(), ++iti; it != t.end(); ++it, ++iti )
+	cout << *it << (iti != t.end() ? "," : "");
+      cout << "}";
       return os;
     }
     
