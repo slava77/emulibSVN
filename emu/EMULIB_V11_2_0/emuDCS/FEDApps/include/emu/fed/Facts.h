add78702f6adca3f7cf8342e81ce1f97482d7f3a
(no author) <(no author)>
2012-04-26 15:41:52 +0000

    This commit was manufactured by cvs2svn to create tag 'EMULIB_V11_2_0'.

---
+++ emu/EMULIB_V11_2_0/emuDCS/FEDApps/include/emu/fed/Facts.h
@@ -0,0 +1,12 @@
+/*****************************************************************************\
+* $Id: Facts.h,v 1.4 2010/08/13 03:00:07 paste Exp $
+\*****************************************************************************/
+#ifndef __EMU_FED_FACTS__
+#define __EMU_FED_FACTS__
+
+#include "emu/fed/Fact.h"
+#include "emu/fed/FEDFSMFact.h"
+#include "emu/fed/DDUFMMIRQFact.h"
+#include "emu/fed/DDUFMMResetFact.h"
+
+#endif
