9165d117e906b877eb2d91c162b3e29b097f5dc0
banicz <banicz>
2012-05-31 13:01:25 +0000

    Allow for hyphens in device id.

--- EMU/AFEB/teststand/script/parametersToDB.sh
+++ EMU/AFEB/teststand/script/parametersToDB.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
+
+# This script should be redundant as the analysis program already produces the same output, namely a summary plain text file for each analyzed device.
+
 if [[ ${#@} -eq 0 ]]; then
     print "No result directory specified. Exiting."
     print "Usage:"
@@ -13,7 +16,7 @@ cd ${BUILD_HOME}/AFEB/teststand/xml
 
 for RESULTDIR in $@; do
     xsltproc failedCutsListerGenerator.xsl ${RESULTDIR}/analyzed/SelectionCuts.xml > failedCutsList.xsl
-    for DEVICEFILE in ${RESULTDIR}/analyzed/[0-9]*.xml; do
+    for DEVICEFILE in ${RESULTDIR}/analyzed/[0-9\-]*.xml; do
 	xsltproc parametersToDB.xsl $DEVICEFILE > ${DEVICEFILE:r}.txt
 	print "Wrote ${DEVICEFILE:r}.txt"
     done
