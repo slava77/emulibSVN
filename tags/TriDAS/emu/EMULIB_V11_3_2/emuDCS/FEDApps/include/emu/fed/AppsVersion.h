55d276be29eaa081b89897a97b556c25e7468efc
(no author) <(no author)>
2012-06-29 12:13:29 +0000

    This commit was manufactured by cvs2svn to create tag 'EMULIB_V11_3_2'.

---
+++ tags/TriDAS/emu/EMULIB_V11_3_2/emuDCS/FEDApps/include/emu/fed/AppsVersion.h
@@ -0,0 +1,28 @@
+/*****************************************************************************\
+* $Id: AppsVersion.h,v 1.76 2012/06/29 11:12:31 banicz Exp $
+\*****************************************************************************/
+
+#ifndef __EMU_FED_APPSVERSION_H__
+#define __EMU_FED_APPSVERSION_H__
+
+#include "config/PackageInfo.h"
+
+namespace EmuFEDApps {
+	const std::string package     = "EmuFEDApps";
+	const std::string versions    = "11.03.02";
+	const std::string description = "Emu (CSC) FED XDAQ Applications";
+
+	const std::string summary     = "Applications to control and monitor the Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
+	const std::string authors     = "Phillip Killewald, Stan Durkin, Jason Gilmore, Jianhui Gu";
+	const std::string link        = "http://www.physics.ohio-state.edu/~cms";
+
+	config::PackageInfo getPackageInfo();
+
+	void checkPackageDependencies()
+	throw (config::PackageInfo::VersionException);
+
+	std::set<std::string, std::less<std::string> > getPackageDependencies();
+}
+
+#endif
+
